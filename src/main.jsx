f8589d319eb1a6db9d3c78d2520df1b0e217e9d1
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-15 14:30:14 -0400

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 import { SimpleForm } from './02-useEffect/SimpleForm';
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
 import { HookApp } from './HookApp';
 import './index.css';
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     // <FormWithCustomHook />
     // <CounterWithCustomHook />
-    <MultipleCustomHooks />
+    // <MultipleCustomHooks />
+    <FocusScreen />
   // </React.StrictMode>
 )
