e60cab72259465e1d485a419dbebc18ece2101a9
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-15 17:10:13 -0400

    practica memo

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import { Layout } from './05-useLayoutEffect/Layout';
 import { CallbackHook } from './06-memos/CallbackHook';
 import { MemoHook } from './06-memos/MemoHook';
 import { Memorize } from './06-memos/Memorize';
+import { Padre } from './07-tarea-memo/Padre';
 import { HookApp } from './HookApp';
 import './index.css';
 
@@ -22,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     // <Layout />
     // <Memorize />
     // <MemoHook />
-    <CallbackHook />
+    // <CallbackHook />
+    <Padre />
   // </React.StrictMode>
 )
