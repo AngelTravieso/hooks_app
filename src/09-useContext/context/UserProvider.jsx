3a88a67c8ed51d09cb88aa0392d86c3608fb0621
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-19 09:03:04 -0400

    useContext

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user = {
-    id: 123,
-    name: 'Angel Trvieso',
-    email: 'correo@correo.com',
-}
+
+// const user = {
+//     id: 123,
+//     name: 'Angel Trvieso',
+//     email: 'correo@correo.com',
+// }
 
 
 export const UserProvider = ({ children }) => {
+
+  const [user, setUser] = useState();
+
+
   return (
-    <UserContext.Provider value={{ hola: 'Mundo', user: user }}>
+    <UserContext.Provider value={{ user, setUser }}>
         { children }
     </UserContext.Provider>
   )
